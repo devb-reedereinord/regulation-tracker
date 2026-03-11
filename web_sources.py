b4262e1833559149581912429e2627c5cd2794e2
devb-reedereinord <bdebdip22@gmail.com>
2026-03-11 11:26:33 +0100

    Add Gard Shipping Changes 2026 to WEB_SOURCES

--- web_sources.py
+++ web_sources.py
@@ -3,6 +3,14 @@ Registry of maritime regulatory sources to monitor
 """
 
 WEB_SOURCES = [
+    WEB_SOURCES = [
+    {
+        "name": "Gard Shipping Changes 2026",
+        "base_url": "https://gard.no/en/insights/what-is-changing-for-shipping-in-2026-1/",
+        "type": "gard_digest",
+        "publisher": "Gard",
+        "jurisdiction": "Global",
+    },
 
     {
         "name": "DNV Technical Regulatory News",
