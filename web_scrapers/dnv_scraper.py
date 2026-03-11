92f7963e231626abb4007e1e60867358e86c4c02
devb-reedereinord <bdebdip22@gmail.com>
2026-03-11 10:56:34 +0100

    Add DNV article discovery scraper function

---
+++ web_scrapers/dnv_scraper.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def discover_dnv_articles(url):
+
+    r = requests.get(url, timeout=30)
+    r.raise_for_status()
+
+    soup = BeautifulSoup(r.text, "lxml")
+
+    links = []
+
+    for a in soup.select("a"):
+
+        href = a.get("href")
+
+        if not href:
+            continue
+
+        if "/maritime/technical-regulatory-news/" in href:
+
+            if href.startswith("/"):
+                href = "https://www.dnv.com" + href
+
+            links.append(href)
+
+    return list(set(links))
