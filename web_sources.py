985c3c5baad5886434c7c9ca565171bb588e2bda
devb-reedereinord <bdebdip22@gmail.com>
2026-03-11 10:54:40 +0100

    Update regulatory sources list and types

--- web_sources.py
+++ web_sources.py
@@ -1,13 +1,31 @@
 """
-List of regulatory websites to monitor
+Registry of maritime regulatory sources to monitor
 """
 
 WEB_SOURCES = [
+
     {
         "name": "DNV Technical Regulatory News",
         "base_url": "https://www.dnv.com/maritime/technical-regulatory-news/",
-        "type": "index_page",
+        "type": "dnv_index",
         "publisher": "DNV",
         "jurisdiction": "Global"
+    },
+
+    {
+        "name": "IMO News",
+        "base_url": "https://www.imo.org/en/MediaCentre/Pages/WhatsNew.aspx",
+        "type": "imo_news",
+        "publisher": "IMO",
+        "jurisdiction": "Global"
+    },
+
+    {
+        "name": "EU Maritime News",
+        "base_url": "https://transport.ec.europa.eu/news-events/news_en",
+        "type": "eu_news",
+        "publisher": "EU",
+        "jurisdiction": "EU"
     }
+
 ]
