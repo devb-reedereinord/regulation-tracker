f549d8074aa52b087a2168e14112665c8ccc2c5c
devb-reedereinord <bdebdip22@gmail.com>
2026-03-11 10:41:34 +0100

    Add list of regulatory websites to monitor

---
+++ web_sources.py
@@ -0,0 +1,13 @@
+"""
+List of regulatory websites to monitor
+"""
+
+WEB_SOURCES = [
+    {
+        "name": "DNV Technical Regulatory News",
+        "base_url": "https://www.dnv.com/maritime/technical-regulatory-news/",
+        "type": "index_page",
+        "publisher": "DNV",
+        "jurisdiction": "Global"
+    }
+]
